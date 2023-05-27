0720e8dc05d281ce9f3d8f2e430f471cae4d40a7
Raul <raul.marks@outlook.com>
2023-05-27 16:43:38 +0300

    Added product CRUD

---
+++ FMS.Retail/Server/Features/Products/DeleteProductEndpoint.cs
@@ -0,0 +1,32 @@
+﻿using Ardalis.ApiEndpoints;
+using FMS.Retail.DAL;
+using FMS.Retail.Domain.Model;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FMS.Retail.Server.Features.Products;
+
+public class DeleteProductEndpoint : EndpointBaseAsync.WithRequest<int>.WithActionResult
+{
+    private readonly FMSRetailContext _context;
+
+    public DeleteProductEndpoint(FMSRetailContext context)
+    {
+        _context = context;
+    }
+
+    [HttpDelete("/product/{id}")]
+    public override async Task<ActionResult> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        Product? productToDelete = await _context.Products.FindAsync(id, cancellationToken);
+
+        if (productToDelete is null)
+        {
+            return BadRequest("Product could not be found.");
+        }
+
+        _context.Remove(productToDelete);
+        await _context.SaveChangesAsync();
+
+        return Ok();
+    }
+}
---
+++ FMS.Retail/Server/Features/Products/GetProductEndpoint.cs
@@ -0,0 +1,35 @@
+﻿using Ardalis.ApiEndpoints;
+using FMS.Retail.DAL;
+using FMS.Retail.Shared.Features.Products;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace FMS.Retail.Server.Features.Products;
+
+public class GetProductEndpoint : EndpointBaseAsync.WithRequest<int>.WithActionResult<ProductModel>
+{
+    private readonly FMSRetailContext _context;
+
+    public GetProductEndpoint(FMSRetailContext context)
+    {
+        _context = context;
+    }
+
+    [HttpGet("/product/{id}")]
+    public override async Task<ActionResult<ProductModel>> HandleAsync(int id, CancellationToken cancellationToken = default)
+    {
+        var model = await _context.Products
+            .AsNoTracking()
+            .Where(p  => p.Id == id)
+            .Select(p => new ProductModel
+            {
+                Id = p.Id,
+                Code = p.Code,
+                Name = p.Name,
+                Price = p.Price
+            })
+            .FirstOrDefaultAsync();
+
+        return model is not null ? Ok(model) : BadRequest("Product could not be found.");
+    }
+}
